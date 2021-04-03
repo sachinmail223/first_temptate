472a04e1e4875837c4560381ab115846fa91a450
sachin <sachinmail223@gmail.com>
2021-04-03 15:18:18 +0530

    footer2.0- commit

--- template/src/components/Footer.js
+++ template/src/components/Footer.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import "./Footer.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars} from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faTwitter, faInstagram, faPinterestP, faGoogle} from '@fortawesome/free-brands-svg-icons' 
+
 const Footer = () => {
     return (
         <div className="footer-div container-fluid   ">
@@ -7,10 +12,26 @@ const Footer = () => {
             <div className="row h-100 justify-content-between">
                 <div className="col-md-5 pt-5">
                     <h1 className="font-weight-bold pb-3">BSK,s</h1>
-                    <p className="font-weight-bold lead text-light">In the mean time shall soon find out the cause of this: What was the epicurus towards their children. </p>
+                    <p className="font-weight-bold pb-md-5 lead text-light">In the mean time shall soon find out the cause of this: What was the epicurus towards their children. </p>
                     
                 </div>
-                <div className="col-md-5"></div>
+                <div className="col-md-5 pt-5 pb-5">  
+                    <a href="#" className="col-2 lead  ">
+                    <FontAwesomeIcon icon={faTwitter} className="FaBars social-icon"/>
+                    </a>
+                    <a href="#" className="col-2 lead">
+                    <FontAwesomeIcon icon={faFacebookF} className="FaBars social-icon"/>
+                    </a>
+                    <a href="#" className="col-2 lead">
+                    <FontAwesomeIcon icon={faInstagram} className="FaBars social-icon"/>
+                    </a>
+                    <a href="#" className="col-2 lead">
+                    <FontAwesomeIcon icon={faPinterestP} className="FaBars social-icon"/>
+                    </a>
+                    <a href="#" className="col-2 lead">
+                    <FontAwesomeIcon icon={faGoogle} className="FaBars social-icon"/>
+                    </a>
+                </div>
                 <div className="col-md-5 align-self-end pb-4">
                 <p className="font-weight-bold text-light">Copyright ©2021 All rights reserved | This template is made with by BSK's. </p>
                 </div>
